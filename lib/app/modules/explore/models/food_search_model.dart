912d623d02978838819895913dab5f242955a33c
cmwuaan <minhquanrbpk@gmail.com>
2023-12-09 03:23:48 +0700

    setup model and data

---
+++ lib/app/modules/explore/models/food_search_model.dart
@@ -0,0 +1,9 @@
+class FoodSearchModel {
+  const FoodSearchModel({
+    required this.title,
+    required this.imageUrl,
+  });
+
+  final String title;
+  final String imageUrl;
+}
--- lib/core/constants/strings.dart
+++ lib/core/constants/strings.dart
@@ -1,4 +1,50 @@
 class Strings {
   static const String appName = 'CookHub';
   static const String hello = 'Hello';
+
+  // Explore Screen
+  static const String exploreTitle = 'Explore';
+  static const String filterTitle = 'Filter';
+  static const String searchBarTitle = 'Search recipes, ingredients';
+  static const String quickSearch = 'Quick search';
+  static const String popularTag = 'Popular tags';
+  static const String cookTime = 'Cook time';
+  static const String difficultyTitle = 'Difficulty';
+  static const String dishTypeTitle = 'Dish type';
+  static const String cuisineTitle = 'Cuisine';
+
+  static const String lunch = 'Lunch';
+  static const String vegan = 'Vegan';
+  static const String western = 'Western';
+  static const String soup = 'Soup';
+  static const String thai = 'Thai';
+  static const String dinner = 'Dinner';
+
+  static const String vegetarian = 'Vegetarian';
+  static const String breakfast = 'Breakfast';
+  static const String fastCooking = 'Fast cooking';
+  static const String lowFat = 'Low fat';
+  static const String dessert = 'Dessert';
+  static const String christmas = 'Christmas';
+
+  static const String easy = "Easy";
+  static const String medium = "Medium";
+  static const String likePro = "Like a PRO";
+
+  static const String brunch = "Brunch";
+  static const String snack = "Snack";
+  static const String appetizers = "Appertizers";
+
+  static const String vietnamese = "Vietnamese";
+  static const String korean = "Korean";
+  static const String german = "German";
+  static const String chinese = "Chinese";
+  static const String british = "British";
+  static const String indian = "Indian";
+  static const String indonesia = "Indonesia";
+  static const String mexican = "Mexican";
+  static const String japanese = "Japanese";
+  static const String russia = "Russia";
+
+  static const String seeMore = "See more";
 }
